b1f27db5c7ac22b52ddfda500760a3c438734376
user.email <regacho.marcjulius@gmail.com>
2025-06-03 22:03:04 +0800

    minor updates

--- scripts/lessons/lessons_list.gd
+++ scripts/lessons/lessons_list.gd
@@ -2,11 +2,9 @@ extends Control
 
 func _ready():
 	# calls the other parent from the main parent
-	var first_lessons = $Lessons1
-	var second_lessons = $Lessons2
-	second_lessons.visible = false
+	var lessons_list = $VBoxContainer/Lessons
 	#	it gets the list of children of another parent
-	for child in first_lessons.get_children():
+	for child in lessons_list.get_children():
 #		make sure the child is a button
 		if child is Button:
 			child.pressed.connect(func() : _on_pressed(child.name))
@@ -15,10 +13,5 @@ func _on_pressed(button_name):
 	print("Name: ", button_name)
 
 func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	print("back")
-
-	#match button_name:
-		#"Button":
-			#print("Do action for Button 1")
-		#"Button2":
-			#print("Do action for Button 2")
