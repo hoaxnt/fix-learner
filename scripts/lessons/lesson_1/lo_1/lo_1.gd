f82a9a06fe20f543d3b01596f99ad432c9398ed4
user.email <regacho.marcjulius@gmail.com>
2025-06-09 19:23:18 +0800

    added lo1 assets

--- scripts/lessons/lesson_1/lo_1/lo_1.gd
+++ scripts/lessons/lesson_1/lo_1/lo_1.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var dialogue_box = $CanvasLayer/UI/DialogueBox
 @onready var dialogue = $CanvasLayer/UI/DialogueBox/MarginContainer/VBoxContainer/Dialogue
 @onready var prof = $Prof
+@onready var tool_preview = $ToolPreview
+@onready var choices_box = $CanvasLayer/UI/ChoicesBox
 
 var dialogue_data = [
 		"Hi there, students of computer system servicing!",
@@ -11,17 +13,46 @@ var dialogue_data = [
 		"In this lesson, you'll explore specifications of materials and components like wires, resistors, and integrated circuits",
 		"So lets get started!",
 #		Item description
-		"It conduct material insulated with plastic, used to transmit electrical current from one component to another in a circuit."
+		"[b]Wires[/b] are conductive materials insulated with plastic, used to transmit electrical current from one component to another in a circuit.",
+		"[b]Cables[/b] are groups of two or more insulated wires bundled together to transmit electrical power or data signals.",
+		"[b]Electrical[/b] tape is an insulating tape made of plastic or vinyl, used to wrap and protect electrical wires and connections from moisture and abrasion.",
+		"[b]Resistors[/b] are passive electrical components that limit or regulate the flow of electrical current in a circuit.",
+		"[b]Capacitors[/b] are electronic components that store and release electrical energy.",
+		"[b]Integrated[/b] circuits are microelectronic circuits consisting of semiconductor devices and passive components built onto a single chip.",
+		"[b]Diodes[/b] are semiconductor devices that allow current to flow in one direction only.",
+		"[b]Transistors[/b] are semiconductor devices used to amplify or switch electronic signals.",
+		"Okay, it's quiz time! Are you ready?"
 ]
+
+var textures = {
+	
+}
 
 var index := 0
 
 func _ready() -> void:
+	tool_preview.visible = false
 	dialogue.text = dialogue_data[index]
 
 func _on_pressed() -> void:
 	index += 1
+
 	if index < dialogue_data.size():
-			dialogue.text = dialogue_data[index]
+		dialogue.text = dialogue_data[index]
+
+		if index >= 5:
+			prof.position = Vector2(-680.0, 0.0)
+			tool_preview.visible = true
+
+			# Dynamically change texture based on index
+			var image_path = "res://assets/images/lesson_1/lo_1/tool_%d.png" % (index - 4)
+			var new_texture = load(image_path)
+			
+			if new_texture:
+				tool_preview.texture = new_texture
+			else:
+				tool_preview.texture = null
+				choices_box.visible = true
+				print("❌ Could not load texture:", image_path)
 	else:
-			prof.position = Vector2(-730.0, 0.0)
+		print("END")
