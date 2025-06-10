aa6ed3754e15920e1bc60a764674d35d29d5aaf9
user.email <regacho.marcjulius@gmail.com>
2025-06-10 12:51:56 +0800

    renamed assets, scenes, and scripts

--- scripts/lessons/lessons_list.gd
+++ scripts/lessons/lessons_list.gd
@@ -9,17 +9,17 @@ func _ready():
 func _on_pressed(button_name):
 	print(button_name)
 	match button_name:
-		"1": get_tree().change_scene_to_file("res://scenes/lessons/lesson_1/lesson_1.tscn")
-		"2": get_tree().change_scene_to_file("res://scenes/lessons/lesson_2/lesson_2.tscn")
-		"3": get_tree().change_scene_to_file("res://scenes/lessons/lesson_3/lesson_3.tscn")
-		"4": get_tree().change_scene_to_file("res://scenes/lessons/lesson_4/lesson_4.tscn")
-		"5": get_tree().change_scene_to_file("res://scenes/lessons/lesson_5/lesson_5.tscn")
-		"6": get_tree().change_scene_to_file("res://scenes/lessons/lesson_6/lesson_6.tscn")
-		"7": get_tree().change_scene_to_file("res://scenes/lessons/lesson_7/lesson_7.tscn")
-		"8": get_tree().change_scene_to_file("res://scenes/lessons/lesson_8/lesson_8.tscn")
-		"9": get_tree().change_scene_to_file("res://scenes/lessons/lesson_9/lesson_9.tscn")
-		"10": get_tree().change_scene_to_file("res://scenes/lessons/lesson_10/lesson_10.tscn")
-		"11": get_tree().change_scene_to_file("res://scenes/lessons/lesson_11/lesson_11.tscn")
+		"1": get_tree().change_scene_to_file("res://scenes/lessons/lesson1/lesson1.tscn")
+		"2": get_tree().change_scene_to_file("res://scenes/lessons/lesson2/lesson2.tscn")
+		"3": get_tree().change_scene_to_file("res://scenes/lessons/lesson3/lesson3.tscn")
+		"4": get_tree().change_scene_to_file("res://scenes/lessons/lesson4/lesson4.tscn")
+		"5": get_tree().change_scene_to_file("res://scenes/lessons/lesson5/lesson5.tscn")
+		"6": get_tree().change_scene_to_file("res://scenes/lessons/lesson6/lesson6.tscn")
+		"7": get_tree().change_scene_to_file("res://scenes/lessons/lesson7/lesson7.tscn")
+		"8": get_tree().change_scene_to_file("res://scenes/lessons/lesson8/lesson8.tscn")
+		"9": get_tree().change_scene_to_file("res://scenes/lessons/lesson9/lesson9.tscn")
+		"10": get_tree().change_scene_to_file("res://scenes/lessons/lesson10/lesson10.tscn")
+		"11": get_tree().change_scene_to_file("res://scenes/lessons/lesson11/lesson11.tscn")
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
