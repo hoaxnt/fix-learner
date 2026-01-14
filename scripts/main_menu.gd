b1c2e20e259c654d3df7443436910aa8026111c5
user.email <regacho.marcjulius@gmail.com>
2026-01-14 14:37:39 +0800

    minor fixes

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,11 +1,20 @@
-extends Button
+extends Control
 
-func _on_sound_pressed() -> void:
-	print("sound")
-
+func _on_learn_button_pressed() -> void:
+	load_scene("res://scenes/lessons/lessons_list.tscn")
+	
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+	
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+	
+func _on_accept_button_pressed() -> void:
+	get_child(2).visible = false
+	
+func _on_decline_button_pressed() -> void:
+	get_tree().quit()
+	
 func load_scene(path: String):
 	var scene = load(path)
 	get_tree().change_scene_to_packed(scene)
-	
-func _on_learn_button_pressed() -> void:
-	load_scene("res://scenes/lessons/lessons_list.tscn")
