439e02ba6136517c4ecd4deba34f60a4d0d1e599
user.email <regacho.marcjulius@gmail.com>
2026-01-28 00:12:25 +0800

    minor fixes

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,16 +1,16 @@
 extends Control
 
+func _ready() -> void:
+	initialize_resources()
+
 func _on_continue_button_pressed() -> void:
 	load_scene("res://scenes/learning_materials/coc_list.tscn")
 	
 func _on_learn_button_pressed() -> void:
+	var user_data : UserData = ResourceLoader.load("user://user_data.tres")
+	user_data.opened_once = true
+	ResourceSaver.save(user_data, "res://user_data.tres")
 	
-	#data.opened_once = not data.opened_once
-	#var result = ResourceSaver.save(data, "user://save_file.tres")
-		#
-	#if result == OK:
-		#print("Game Saved!")
-	#print("Learn Opened Once: ", data.opened_once)
 	load_scene("res://scenes/learning_materials/coc_list.tscn")
 	
 func _on_options_button_pressed() -> void:
@@ -28,3 +28,17 @@ func _on_decline_button_pressed() -> void:
 func load_scene(path: String):
 	var scene = load(path)
 	get_tree().change_scene_to_packed(scene)
+	
+func initialize_resources():
+	var files = ["user://user_data.tres", "user://new_game_data.tres", "user://settings_data.tres"]
+	if not files.all(FileAccess.file_exists):
+		var user_data = UserData.new()
+		var settings_data = SettingsData.new()
+		var new_game_data = NewGameData.new()
+		ResourceSaver.save(user_data, "user://user_data.tres")
+		ResourceSaver.save(settings_data, "user://settings_data.tres")
+		ResourceSaver.save(new_game_data, "user://new_game_data.tres")
+		print("Default data initialized")
+	else:
+		print("Data already exist.")
+		
