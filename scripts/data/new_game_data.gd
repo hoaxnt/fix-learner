21b453e7f6edc145ae5edf6ba4ef70551517616d
user.email <regacho.marcjulius@gmail.com>
2026-01-27 19:06:59 +0800

    minor fixes

--- scripts/data/new_game_data.gd
+++ scripts/data/new_game_data.gd
@@ -1,5 +1,15 @@
 extends Resource
 class_name NewGameData
 
+# COC
 @export var coc_1_unlocked : bool = true
 @export var coc_2_unlocked : bool = false
+
+# COC 1 Lessons
+@export var ohs_procedures : bool = true
+@export var electrical_hazards : bool = false
+@export var assemble_disassemble : bool = false
+@export var creating_bootable_device : bool = false
+@export var installing_softwares : bool = false
+
+#COC 2 Lessons
