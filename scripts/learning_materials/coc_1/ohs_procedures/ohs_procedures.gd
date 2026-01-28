7aedaf5256a005bc1aaf502c161855a9307621bd
user.email <regacho.marcjulius@gmail.com>
2026-01-29 07:10:23 +0800

    minor fixes

--- scripts/learning_materials/coc_1/ohs_procedures/ohs_procedures.gd
+++ scripts/learning_materials/coc_1/ohs_procedures/ohs_procedures.gd
@@ -2,7 +2,19 @@ extends Node
 
 @onready var dialog_box = $CanvasLayer/DialogBox
 @onready var teacher_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var minigame : Node2D = $Minigame
+@onready var minigame_label : Label = $CanvasLayer/MinigameLabel
+
+func _ready() -> void:
+	pass
+	
+	dialog_box.show()
+	teacher_sprite.show()
+	minigame.hide()
+	minigame_label.hide()
 
 func _process(_delta: float) -> void:
 	if dialog_box.current_line_index == 4:
 		teacher_sprite.hide()
+		minigame.show()
+		minigame_label.show()
