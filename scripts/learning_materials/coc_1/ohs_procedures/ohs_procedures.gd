6729da8af2900acff207432ec077d9caff6c2f3e
user.email <regacho.marcjulius@gmail.com>
2026-01-28 14:38:30 +0800

    minor fixes

--- scripts/learning_materials/coc_1/ohs_procedures/ohs_procedures.gd
+++ scripts/learning_materials/coc_1/ohs_procedures/ohs_procedures.gd
@@ -4,13 +4,5 @@ extends Node
 @onready var teacher_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(_delta: float) -> void:
-	print(dialog_box.current_line_index)
-	if dialog_box.current_line_index == 3:
+	if dialog_box.current_line_index == 4:
 		teacher_sprite.hide()
-		
-		var extra_lines: Array[String] = [
-				"Wait! I forgot to tell you something.",
-        "The treasure is hidden behind the [wave]waterfall[/wave]."
-		]
-		dialog_box.update_dialog(extra_lines)
-		print(dialog_box.current_line_index)
