83dba364252a9c86f751ac8f2024eff137b6c1b0
user.email <regacho.marcjulius@gmail.com>
2026-01-29 07:35:53 +0800

    minor fixes

--- scripts/learning_materials/coc_1/coc_1.gd
+++ scripts/learning_materials/coc_1/coc_1.gd
@@ -7,7 +7,7 @@ func _on_ohs_button_pressed() -> void:
 	SceneTransition.change_scene("res://scenes/learning_materials/coc_1/ohs_procedures/ohs_procedures.tscn")
 
 func _on_hazards_button_pressed() -> void:
-	SceneTransition.change_scene("res://scenes/learning_materials/coc_1/ohs_procedures/ohs_procedures.tscn")
+	SceneTransition.change_scene("")
 
 func _on_assemble_button_pressed() -> void:
-	SceneTransition.change_scene("res://scenes/learning_materials/coc_1/ohs_procedures/ohs_procedures.tscn")
+	SceneTransition.change_scene("")
