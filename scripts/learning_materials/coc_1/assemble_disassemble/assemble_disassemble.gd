ddd97eec2673565a415200785a4139d1c8d8ad0e
user.email <regacho.marcjulius@gmail.com>
2026-01-30 22:05:53 +0800

    minor fixes

--- scripts/learning_materials/coc_1/assemble_disassemble/assemble_disassemble.gd
+++ scripts/learning_materials/coc_1/assemble_disassemble/assemble_disassemble.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+@onready var dialog_box : MarginContainer = $CanvasLayer/DialogBox
+@onready var teacher_sprite : Sprite2D = $Teacher
+
+func _process(_delta: float) -> void:
+	await dialog_box.dialog_finished
+	teacher_sprite.hide()
