0bbc49fda6fa36883a891338b955699aface0793
user.email <regacho.marcjulius@gmail.com>
2026-01-30 11:49:23 +0800

    minor fixes

--- scripts/learning_materials/coc_1/electrical_hazards/touch_and_drag_electrical_hazards.gd
+++ scripts/learning_materials/coc_1/electrical_hazards/touch_and_drag_electrical_hazards.gd
@@ -3,6 +3,6 @@ extends Area2D
 @export var item_sprite : Texture2D
 
 @onready var sprite : Sprite2D = $Sprite2D
-
+#NOTE : GENERATE IMAGES OF HAZARDS ONE BY ONE ATLEAST 4
 func _ready() -> void:
 	sprite.texture = item_sprite
