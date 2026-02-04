2de37653c018a7fed5e335cee0e870cef2c82429
user.email <regacho.marcjulius@gmail.com>
2026-02-04 12:01:05 +0800

    minor fixes

--- scripts/learning_materials/coc_1/bootable_device/bootable_device.gd
+++ scripts/learning_materials/coc_1/bootable_device/bootable_device.gd
@@ -3,9 +3,9 @@ extends Node2D
 @onready var background : Sprite2D = $Background
 @onready var dialog_box : MarginContainer = $CanvasLayer/DialogBox
 @onready var teacher_sprite : Sprite2D = $Teacher
-
+@onready var options_menu : Control = $CanvasLayer/OptionsMenu
 @export var scenes_texture : Array[Texture2D]
-#FIX THE NUMBERING
+
 var current_tex_index : int = 0
 
 var texture_map : Dictionary = {
@@ -28,7 +28,7 @@ var texture_map : Dictionary = {
 	18: 14,
 	19: 15,
 	20: 16,
-	21: 17,
+	21: 16,
 	22: 17,
 	23: 18,
 	24: 19,
@@ -49,53 +49,60 @@ func _start_tutorial():
 	teacher_sprite.show()
 
 	var data : Array[String] = [
-	"Well done, student. Now, let’s take it a step further.",
-	"In our next lab, you’ll be preparing a bootable flash drive...",
-	"Pay close attention to the formatting steps.",
-	
-	"Search Rufus and click the link below that says Rufus...",
-	"Now click the rufus-4.12.exe with the standard type.",
-	"You can now see the downloaded setup on the top right corner, but don't click it yet.",
-	"Search Windows 11 ISO on Google Chrome.",
-	"Click Download Windows 11 below.",
-	"After that, you may select the type of OS you want to download; this time we are using Windows 11.",
-	"Click the select download drop down section.",
-	"Now select the Windows 11 Multi-edition ISO for x64 devices.",
-	"Wait for a few seconds...",
-	"Now in this section, we will select the language we will use.",
-	"Choose English (United States).",
-	"And wait a few seconds...",
-	"Click 64-bit Download.",
-	"And you will see on the right top that the download is in progress.",
-	"While the Win11 is downloading, click the Rufus button to be directed to the folder.",
-	"Click the Rufus file...",
-	"And click Open!",
-	"The device name needs to be correct; the USB input listed here must be correct for the files to be extracted there.",
-	"After securing the USB device, now click 'Select' to find the location of the Windows ISO file.",
-	"Once the Windows ISO file is found, click on the entire ISO file and then click the Open button at the bottom.",
-	"The next step is to click the Start button below to proceed to the next procedure.",
-	"If the Windows User Experience window appears, just click OK.",
-	"Click the OK button again to start extracting our ISO file.",
-	"Just wait until our ISO file is extracted and don't touch it.",
-	"Once our extracted ISO file is finished, just click the Close button at the bottom.",
-	"Now let's go to our PC's file manager and check if the inserted USB files have been extracted correctly.",
-	"Once all the files are okay, it can be used to reformat the computer.",
-	"WELL DONE!",
-	"LETS MOVE ON"
-	
-]
+		"Now that you've mastered the basics, let’s move on to a critical technical skill.",
+		"In this lab, you will learn how to transform a standard USB drive into a bootable Windows 11 installation tool.",
+		"Precision is key here follow every step carefully to ensure the boot media works correctly.",
+		"Search Rufus and click the link below that says Rufus...",
+		"Now click the rufus-4.12.exe with the standard type.",
+		"You can now see the downloaded setup on the top right corner, but don't click it yet.",
+		"Search Windows 11 ISO on Google Chrome.",
+		"Click Download Windows 11 below.",
+		"After that, you may select the type of OS you want to download; this time we are using Windows 11.",
+		"Click the select download drop down section.",
+		"Now select the Windows 11 Multi-edition ISO for x64 devices.",
+		"Wait for a few seconds...",
+		"Now in this section, we will select the language we will use.",
+		"Choose English (United States).",
+		"And wait a few seconds...",
+		"Click 64-bit Download.",
+		"And you will see on the right top that the download is in progress.",
+		"While the Win11 is downloading, click the Rufus button to be directed to the folder.",
+		"Click the Rufus file...",
+		"And click Open!",
+		"The device name needs to be correct; the USB input listed here must be correct for the files to be extracted there.",
+		"After securing the USB device, now click 'Select' to find the location of the Windows ISO file.",
+		"Once the Windows ISO file is found, click on the entire ISO file and then click the Open button at the bottom.",
+		"The next step is to click the Start button below to proceed to the next procedure.",
+		"If the Windows User Experience window appears, just click OK.",
+		"Click the OK button again to start extracting our ISO file.",
+		"Just wait until our ISO file is extracted and don't touch it.",
+		"Once our extracted ISO file is finished, just click the Close button at the bottom.",
+		"Now let's go to our PC's file manager and check if the inserted USB files have been extracted correctly.",
+		"Once all the files are confirmed, your bootable drive is officially ready to reformat a computer!",
+		"You’ve successfully navigated the bootable drive creation process.",
+		"Prepare yourself it's time to head to the next challenge!"
+		]
 
 	if scenes_texture.size() > 0:
 		background.texture = scenes_texture[0]
 		
 	dialog_box.update_dialog("Teacher", data)
 	
+func _end():
+	SceneTransition.change_scene("res://scenes/learning_materials/coc_1/lessons_list_1.tscn")
+	
 func _on_line_changed(index: int):
-	teacher_sprite.visible = (index < 3)
-	teacher_sprite.visible = (index >= 29)
-
+	teacher_sprite.visible = (index <= 3)
+	if index >= 3:
+		teacher_sprite.visible = (index >= 29)
+	print(index)
+	if index >= 31:
+		dialog_box.dialog_finished.connect(_end)
 	if texture_map.has(index):
 		current_tex_index = texture_map[index]
 	
 	if current_tex_index < scenes_texture.size():
 		background.texture = scenes_texture[current_tex_index]
+
+func _on_menu_button_button_up() -> void:
+	options_menu.show()
--- scripts/utils/scene_transition.gd
+++ scripts/utils/scene_transition.gd
@@ -1,14 +1,40 @@
 extends CanvasLayer
 
-@onready var animation_player = $AnimationPlayer
-@onready var color_rect = $ColorRect
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var color_rect: ColorRect = $ColorRect
 
-func _ready():
-	color_rect.modulate.a = 0
+var is_changing: bool = false
+
+func _ready() -> void:
+	color_rect.visible = false
 	color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
 
-func change_scene(target_scene_path: String):
-		animation_player.play("fade_to_black")
-		await animation_player.animation_finished
-		get_tree().change_scene_to_file(target_scene_path)
-		animation_player.play("fade_to_normal")
+func change_scene(target_scene_path: String) -> void:
+	if is_changing:
+		return
+	
+	is_changing = true
+	
+	color_rect.mouse_filter = Control.MOUSE_FILTER_STOP
+	color_rect.visible = true
+	
+	animation_player.play("fade_to_black")
+	await animation_player.animation_finished
+	
+	var path = target_scene_path.strip_edges()
+	var error = get_tree().change_scene_to_file(path)
+	
+	if error != OK:
+		push_error("SCENE CHANGER ERROR: Could not load path: " + path)
+		is_changing = false
+		color_rect.visible = false
+		return
+
+	await get_tree().process_frame
+	
+	animation_player.play("fade_to_normal")
+	await animation_player.animation_finished
+	
+	color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	color_rect.visible = false
+	is_changing = false
