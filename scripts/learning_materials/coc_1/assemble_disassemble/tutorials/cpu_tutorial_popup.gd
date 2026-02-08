2775da44a867ab3573e550777ca50bafc49f3828
user.email <regacho.marcjulius@gmail.com>
2026-02-09 03:20:59 +0800

    minor fixes

--- scripts/learning_materials/coc_1/assemble_disassemble/tutorials/cpu_tutorial_popup.gd
+++ scripts/learning_materials/coc_1/assemble_disassemble/tutorials/cpu_tutorial_popup.gd
@@ -4,15 +4,19 @@ extends Node2D
 @onready var button_2 : Button = $CanvasLayer/Button2
 @onready var dialog_box : MarginContainer = $CanvasLayer/DialogBox
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	button_2.hide()
-
 
 func _on_button_1_button_up() -> void:
 	button_2.show()
 	button_1.hide()
 
 func _on_button_2_button_up() -> void:
+	button_2.hide()
+	
 	var data : Array[String] = ["You can now put the fan", "That's nice!"]
 	dialog_box.update_dialog("Teacher", data)
+	
+	await dialog_box.dialog_finished
+	
+	queue_free()
