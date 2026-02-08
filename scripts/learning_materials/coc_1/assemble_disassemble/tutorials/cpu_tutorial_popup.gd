ad95a27037d18bc598dc956e4d3cfa8b85833b2b
user.email <regacho.marcjulius@gmail.com>
2026-02-09 02:20:28 +0800

    minor fixes

--- scripts/learning_materials/coc_1/assemble_disassemble/tutorials/cpu_tutorial_popup.gd
+++ scripts/learning_materials/coc_1/assemble_disassemble/tutorials/cpu_tutorial_popup.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
+@onready var button_1 : Button = $CanvasLayer/Button1
+@onready var button_2 : Button = $CanvasLayer/Button2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	button_2.hide()
+
+
+func _on_button_1_button_up() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_button_2_button_up() -> void:
+	pass # Replace with function body.
