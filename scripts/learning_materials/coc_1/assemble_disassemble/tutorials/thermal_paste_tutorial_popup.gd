18642bc012193207c7ba56e943b0923306007f13
user.email <regacho.marcjulius@gmail.com>
2026-02-09 16:13:34 +0800

    minor fixes

---
+++ scripts/learning_materials/coc_1/assemble_disassemble/tutorials/thermal_paste_tutorial_popup.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+@onready var dialog_box : MarginContainer = $DialogBox
+@onready var button_1 : Button = $Button1
+@onready var button_2 : Button = $Button2
+
+func _ready() -> void:
+	pass
+	
+	
