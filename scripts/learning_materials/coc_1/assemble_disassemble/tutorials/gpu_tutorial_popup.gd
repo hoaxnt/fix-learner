1d2d62b9a3c501693eede4a14a9743e42a4b4356
user.email <regacho.marcjulius@gmail.com>
2026-02-09 18:27:51 +0800

    minor fixes

--- scripts/learning_materials/coc_1/assemble_disassemble/tutorials/gpu_tutorial_popup.gd
+++ scripts/learning_materials/coc_1/assemble_disassemble/tutorials/gpu_tutorial_popup.gd
@@ -1,11 +1,26 @@
 extends CanvasLayer
 
+@onready var button_1 : Button = $Button1
+@onready var button_2 : Button = $Button2
+@onready var background : TextureRect = $Background
+@onready var dialog_box : MarginContainer = $DialogBox
 
-# Called when the node enters the scene tree for the first time.
+@onready var image_2 : Texture2D = preload("res://assets/tutorials/assemble_disassemble/gpu_tutorial/2.png")
+
 func _ready() -> void:
-	pass # Replace with function body.
+	await dialog_box.dialog_finished
+	button_1.show()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_button_1_button_up() -> void:
+	button_1.hide()
+	button_2.show()
+	
+func _on_button_2_button_up() -> void:
+	background.texture = image_2
+	button_2.hide()
+	var data : Array[String] = ["After hearing the click sound, you are ready to proceed to the next step."]
+	dialog_box.update_dialog("Teacher", data)
+	
+	await dialog_box.dialog_finished
+	
+	queue_free()
