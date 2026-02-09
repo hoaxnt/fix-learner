cec8e163f2889518ae1fa39bbd92f233b14dc545
user.email <regacho.marcjulius@gmail.com>
2026-02-09 18:16:16 +0800

    minor fixes

--- scripts/learning_materials/coc_1/assemble_disassemble/tutorials/ram_tutorial_popup.gd
+++ scripts/learning_materials/coc_1/assemble_disassemble/tutorials/ram_tutorial_popup.gd
@@ -1,10 +1,26 @@
 extends CanvasLayer
 
+@onready var button_1 : Button = $Button1
+@onready var button_2 : Button = $Button2
+@onready var background : TextureRect = $Background
+@onready var dialog_box : MarginContainer = $DialogBox
+
+@onready var image_2 : Texture2D = preload("res://assets/tutorials/assemble_disassemble/ram_tutorial/2.png")
+
 func _ready() -> void:
-	pass # Replace with function body.
+	await dialog_box.dialog_finished
+	button_1.show()
 
 func _on_button_1_button_up() -> void:
-	pass # Replace with function body.
-
+	button_1.hide()
+	button_2.show()
+	
 func _on_button_2_button_up() -> void:
-	pass # Replace with function body.
+	background.texture = image_2
+	button_2.hide()
+	var data : Array[String] = ["Correctas"]
+	dialog_box.update_dialog("Teacher", data)
+	
+	await dialog_box.dialog_finished
+	
+	queue_free()
