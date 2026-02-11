97e9274d9a163054956feb50a9486e04169c6594
user.email <regacho.marcjulius@gmail.com>
2026-02-11 21:05:21 +0800

    minor fixes

---
+++ scripts/learning_materials/coc_1/assemble_disassemble/tutorials/mobo_to_unit_tutorial_popup.gd
@@ -0,0 +1,87 @@
+extends CanvasLayer
+
+@onready var background : TextureRect = $Background
+@onready var dialog_box : MarginContainer = $DialogBox
+
+@onready var button_1 : Button = $Button1
+@onready var button_2 : Button = $Button2
+@onready var button_3 : Button = $Button3
+@onready var button_4 : Button = $Button4
+@onready var screw_button : Button = $ItemList/ScrewButton
+@onready var screw_driver_button : Button = $ItemList/ScrewdriverButton
+
+@onready var image_1 : Texture2D = preload("res://assets/tutorials/assemble_disassemble/cpu_fan_tutorial/1.png")
+@onready var image_2 : Texture2D = preload("res://assets/tutorials/assemble_disassemble/cpu_fan_tutorial/2.png")
+@onready var image_3 : Texture2D = preload("res://assets/tutorials/assemble_disassemble/cpu_fan_tutorial/3.png")
+@onready var image_4 : Texture2D = preload("res://assets/tutorials/assemble_disassemble/cpu_fan_tutorial/4.png")
+
+var all_screw_placed : bool = false
+
+func _on_button_1_button_up() -> void:
+	background.texture = image_1
+	button_1.hide()
+	button_2.show()
+
+func _on_button_2_button_up() -> void:
+	background.texture = image_2
+	button_2.hide()
+	button_3.show()
+
+func _on_button_3_button_up() -> void:
+	if all_screw_placed:
+		background.texture = image_c4
+		button_3.hide()
+
+		var final_data : Array[String] = ["Alright, you did it correctly. Let’s now proceed with the next one"]
+		dialog_box.update_dialog("Teacher", final_data)
+		await dialog_box.dialog_finished
+		queue_free()
+		return
+	background.texture = image_3
+	button_3.hide()
+	button_4.show()
+
+func _on_button_4_button_up() -> void:
+	if all_screw_placed:
+		background.texture = image_c2
+		button_4.hide()
+		button_2.text = "3"
+		button_2.show()
+		return
+		
+	background.texture = image_4
+	button_4.hide()
+	var data : Array[String] = ["All screws are positioned correctly. Use the screwdriver to tighten them securely."]
+	dialog_box.update_dialog("Teacher", data) 
+	all_screw_placed = true
+
+func _on_screw_button_button_up() -> void:
+	# 1. Logic for the Screw Button itself
+	screw_button.toggle_mode = true
+	screw_button.button_pressed = true # Keep this one down
+	
+	# 2. Untoggle the Screwdriver (the "vice versa" part)
+	screw_driver_button.button_pressed = false
+	
+	# 3. Functional logic
+	if not all_screw_placed:
+		button_1.show()
+
+func _on_screwdriver_button_button_up() -> void:
+	# 1. Logic for the Screwdriver Button itself
+	screw_driver_button.toggle_mode = true
+	screw_driver_button.button_pressed = true # Keep this one down
+	
+	# 2. Untoggle the Screw button
+	screw_button.button_pressed = false
+	
+	# 3. Functional logic
+	if all_screw_placed:
+		# Restart the button sequence for tightening
+		button_1.show()
+	else:
+		# If they haven't finished placing screws, 
+		# pop the screwdriver back up and warn them
+		screw_driver_button.button_pressed = false
+		var warning_data : Array[String] = ["You need to put all the screws in place first before tightening them to ensure the CPU cooler is properly balanced and evenly mounted, which helps apply consistent pressure and improves cooling performance."]
+		dialog_box.update_dialog("Teacher", warning_data)
