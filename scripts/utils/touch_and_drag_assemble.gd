1fe3dca60f392cfdb54af6e8b1a5597693348a29
user.email <regacho.marcjulius@gmail.com>
2026-02-17 10:30:47 +0800

    minor fixes

--- scripts/utils/touch_and_drag_assemble.gd
+++ scripts/utils/touch_and_drag_assemble.gd
@@ -47,7 +47,7 @@ func start_dragging() -> void:
 	z_index = 100
 	offset = global_position - get_global_mouse_position()
 	
-	# --- Wirign PSU LOGIC ---
+	# --- Wirign PSU	 ---
 	if not tutorial_shown and tutorial_scene:
 		var tutorial_instance = tutorial_scene.instantiate()
 		# Add to the CanvasLayer of the current main scene
