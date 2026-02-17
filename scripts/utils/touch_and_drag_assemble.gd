69bd71c7a9d37c35601c92565e705968c5a19fdc
user.email <regacho.marcjulius@gmail.com>
2026-02-17 10:29:36 +0800

    minor fixes

--- scripts/utils/touch_and_drag_assemble.gd
+++ scripts/utils/touch_and_drag_assemble.gd
@@ -3,7 +3,7 @@ extends Area2D
 @export var item_name_string : String = "none"
 @export var item_label : Label
 @export var shadow : Sprite2D
-# NEW: Preload the scene you want to instantiate
+
 @export var tutorial_scene : PackedScene = preload("res://scenes/learning_materials/coc_1/assemble_disassemble/tutorials/wiring_psu_tutorial_popup.tscn")
 
 @onready var user_data = ResourceLoader.load("user://user_data.tres")
