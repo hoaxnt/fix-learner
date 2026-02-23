1c4ebf50de9c1ae93bfc2d6c833f11383cfc97fa
user.email <regacho.marcjulius@gmail.com>
2026-02-23 13:15:44 +0800

    minor fixes

--- scripts/learning_materials/coc_1/assemble_disassemble/tutorials/psu_to_unit_tutorial_popup.gd
+++ scripts/learning_materials/coc_1/assemble_disassemble/tutorials/psu_to_unit_tutorial_popup.gd
@@ -19,34 +19,53 @@ signal wiring_complete
 @onready var image_4 : Texture2D = preload("res://assets/tutorials/assemble_disassemble/psu_to_unit_tutorial/p5.jpg")
 
 var all_screw_placed : bool = false
+var screws_tightened : int = 0
 
 func _ready() -> void:
+	button_1.hide()
 	button_2.hide()
 	button_3.hide()
 	button_4.hide()
-	screw_driver_button.disabled = true
+	dialog_box.hide()
 
 func _on_button_1_pressed() -> void:
-	background.texture = image_1
+	if screw_button.button_pressed:
+		background.texture = image_1
+	
 	button_1.hide()
 	button_2.show()
+	_handle_tightening()
 
 func _on_button_2_pressed() -> void:
-	background.texture = image_2
+	if screw_button.button_pressed:
+		background.texture = image_2
+	
 	button_2.hide()
 	button_3.show()
+	_handle_tightening()
 
 func _on_button_3_pressed() -> void:
-	background.texture = image_3
+	if screw_button.button_pressed:
+		background.texture = image_3
+	
 	button_3.hide()
 	button_4.show()
+	_handle_tightening()
 
 func _on_button_4_pressed() -> void:
-	background.texture = image_4
-	all_screw_placed = true
+	if screw_button.button_pressed:
+		background.texture = image_4
+		all_screw_placed = true
+		screw_driver_button.disabled = false
+	
 	button_4.hide()
-	screw_driver_button.disabled = false
+	_handle_tightening()
 
+func _handle_tightening() -> void:
+	if screw_driver_button.button_pressed and all_screw_placed:
+		screws_tightened += 1
+		if screws_tightened >= 4:
+			_finish_tutorial()
 
 func _on_screw_button_button_up() -> void:
 	screw_button.toggle_mode = true
@@ -56,12 +75,27 @@ func _on_screw_button_button_up() -> void:
 		button_1.show()
 
 func _on_screwdriver_button_button_up() -> void:
+	if not all_screw_placed:
+		screw_driver_button.button_pressed = false
+		var warning_data : Array[String] = ["You need to put all the screws in place first before tightening them."]
+		dialog_box.show()
+		dialog_box.update_dialog("Teacher", warning_data)
+		return
+
 	screw_driver_button.toggle_mode = true
 	screw_driver_button.button_pressed = true
 	screw_button.button_pressed = false
-	if all_screw_placed:
-		button_1.show()
+	button_1.show()
+
+func _finish_tutorial() -> void:
+	dialog_box.show()
+	var final_data : Array[String] = ["Great job! The Power Supply Unit is now securely mounted to the case."]
+	dialog_box.update_dialog("Teacher", final_data)
+	
+	if dialog_box.has_signal("dialog_finished"):
+		await dialog_box.dialog_finished
 	else:
-		screw_driver_button.button_pressed = false
-		var warning_data : Array[String] = ["You need to put all the screws in place first before tightening them."]
-		dialog_box.update_dialog("Teacher", warning_data)
+		await get_tree().create_timer(3.0).timeout
+	
+	wiring_complete.emit()
+	queue_free()
