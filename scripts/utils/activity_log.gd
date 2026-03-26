7006390835f5e9ac58bcc5a934ebf0d3dfa8e9d7
user.email <regacho.marcjulius@gmail.com>
2026-03-26 08:42:17 +0800

    minor fixes

--- scripts/utils/activity_log.gd
+++ scripts/utils/activity_log.gd
@@ -8,3 +8,6 @@ func _on_back_button_pressed() -> void:
 
 func _on_safety_tools_pressed() -> void:
 	SceneTransition.change_scene("res://scenes/utils/activities/ohs_procedure_activity/ohs_procedure_activity.tscn")
+
+func _on_electrical_hazards_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/utils/activities/ohs_procedure_activity/ohs_procedure_activity.tscn")
