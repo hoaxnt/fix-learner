37d02b7079025ba2e9b25b580511219989b15777
user.email <regacho.marcjulius@gmail.com>
2026-04-08 16:19:40 +0800

    minor fixes

--- scripts/utils/activity_log.gd
+++ scripts/utils/activity_log.gd
@@ -4,9 +4,12 @@ extends Control
 
 func _on_back_button_pressed() -> void:
 	SceneTransition.change_scene("res://scenes/main_menu.tscn")
-
+	
 func _on_safety_tools_pressed() -> void:
 	SceneTransition.change_scene("res://scenes/utils/activities/ohs_procedure_activity/ohs_procedure_activity.tscn")
-
+	
 func _on_electrical_hazards_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/utils/activities/electrical_hazards_activity/electrical_hazards_activity.tscn")
+	
+func _on_bootable_device_pressed() -> void:
 	SceneTransition.change_scene("res://scenes/utils/activities/electrical_hazards_activity/electrical_hazards_activity.tscn")
