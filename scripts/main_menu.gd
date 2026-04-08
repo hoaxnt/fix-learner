7d97869530d8f9a7af673ac8b1c6b152dd292a1b
user.email <regacho.marcjulius@gmail.com>
2026-04-08 11:05:59 +0800

    minor fixes

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,8 +3,6 @@ extends Control
 @onready var user_data = ResourceLoader.load("user://user_data.tres")
 @onready var canvas_layer : CanvasLayer = $CanvasLayer
 @onready var user_agreement : Panel = $CanvasLayer/Panel
-@onready var achievements = preload("res://scenes/utils/achievements.tscn")
-@onready var activity_log = preload("res://scenes/utils/activity_log.tscn")
 
 func _ready() -> void:
 	#user_data.opened_once = false
@@ -30,14 +28,9 @@ func _on_options_button_pressed() -> void:
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 	
-
-
-
-
 func _on_accept_button_pressed() -> void:
 	user_data.opened_once = true
 	ResourceSaver.save(user_data, "user://user_data.tres")
-	
 	get_child(2).visible = false
 	
 func _on_decline_button_pressed() -> void:
