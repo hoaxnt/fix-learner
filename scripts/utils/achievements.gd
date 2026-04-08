8d7f3a9bc46d277bb716bc5358c51e9833adc6ec
user.email <regacho.marcjulius@gmail.com>
2026-04-08 11:55:20 +0800

    minor fixes

--- scripts/utils/achievements.gd
+++ scripts/utils/achievements.gd
@@ -2,3 +2,4 @@ extends Control
 
 func _on_close_button_pressed() -> void:
 	SceneTransition.change_scene("res://scenes/main_menu.tscn")
+	
--- scripts/utils/achivement_preview.gd
+++ scripts/utils/achivement_preview.gd
@@ -3,21 +3,5 @@ extends Panel
 @onready var display = $VBoxContainer/Display
 @onready var close_button = $VBoxContainer/CloseButton
 
-# Preloads updated to match your file extensions exactly
-@onready var protocol_master = preload("res://assets/ui/achievement/protocol-master.jpg")
-@onready var hazard_handler = preload("res://assets/ui/achievement/hazard.PNG")
-@onready var core_master = preload("res://assets/ui/achievement/core-master.jpg")
-@onready var cool_operator = preload("res://assets/ui/achievement/cool-operator.jpg")
-@onready var airflow_expert = preload("res://assets/ui/achievement/airflow-expert.PNG")
-@onready var memory_ready = preload("res://assets/ui/achievement/memory-ready.jpg")
-@onready var visual_boost = preload("res://assets/ui/achievement/visual-boost.jpg")
-@onready var speed_driver = preload("res://assets/ui/achievement/speed-driver.jpg")
-@onready var board_secured = preload("res://assets/ui/achievement/board-secured.PNG")
-@onready var power_connected = preload("res://assets/ui/achievement/power-connected.jpg")
-@onready var power_routed = preload("res://assets/ui/achievement/power-routed.jpg")
-@onready var usb_ready = preload("res://assets/ui/achievement/usb-ready.jpg")
-@onready var os_installed = preload("res://assets/ui/achievement/os-installed.jpg")
-
-func _ready() -> void:
-	# Default display
-	display.texture = os_installed
+func _on_close_button_pressed() -> void:
+	get_parent().hide()
