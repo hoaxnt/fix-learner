6be285cb02f8a2fe1a7e2e2e94100797e25b8067
user.email <regacho.marcjulius@gmail.com>
2026-06-03 06:59:49 +0800

    minor fixes

--- scripts/learning_materials/coc_1/coc_1.gd
+++ scripts/learning_materials/coc_1/coc_1.gd
@@ -2,9 +2,14 @@ extends Control
 
 func _on_back_pressed() -> void:
 	SceneTransition.change_scene("res://scenes/learning_materials/coc_list.tscn")
-func _on_wired_connection_button_pressed() -> void:
-	SceneTransition.change_scene("res://scenes/learning_materials/coc_2/wired_connection/wired_connection.tscn")
-func _on_wireless_connection_button_pressed() -> void:
-	SceneTransition.change_scene("res://scenes/learning_materials/coc_2/wireless_connection/wireless_connection.tscn")
-func _on_lan_button_pressed() -> void:
-	SceneTransition.change_scene("res://scenes/learning_materials/coc_2/lan/lan.tscn")
+
+func _on_ohs_button_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/learning_materials/coc_1/ohs_procedures/ohs_procedures.tscn")
+func _on_hazards_button_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/learning_materials/coc_1/electrical_hazards/electrical_hazards.tscn")
+func _on_assemble_button_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/learning_materials/coc_1/assemble_disassemble/assemble_disassemble.tscn")
+func _on_bootable_button_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/learning_materials/coc_1/bootable_device/bootable_device.tscn")
+func _on_software_button_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/learning_materials/coc_1/installing_softwares/installing_softwares.tscn")
--- scripts/learning_materials/coc_2/coc_2.gd
+++ scripts/learning_materials/coc_2/coc_2.gd
@@ -1,17 +1,10 @@
 extends Control
 
-func _ready():
-	var lo_list = get_node("VBoxContainer/L0")
-	for child in lo_list.get_children():
-		if child is Button:
-			child.pressed.connect(func() : _on_pressed(child.name))
-	pass
-	
-func _on_pressed(button_name):
-	match button_name:
-		"1": get_tree().change_scene_to_file("res://scenes/lessons/lesson1/lo1/lo1.tscn")
-		"2": get_tree().change_scene_to_file("res://scenes/lessons/lesson1/lo2/lo2.tscn")
-	
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-	print("back")
+	SceneTransition.change_scene("res://scenes/learning_materials/coc_list.tscn")
+func _on_wired_connection_button_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/learning_materials/coc_2/wired_connection/wired_connection.tscn")
+func _on_wireless_connection_button_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/learning_materials/coc_2/wireless_connection/wireless_connection.tscn")
+func _on_lan_button_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/learning_materials/coc_2/lan/lan.tscn")
